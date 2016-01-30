4f7c34ad0a5961afba8c64f1b4dfb0883821ae3a
ale311 <ale.pulse@gmail.com>
2016-01-30 12:48:29 +0100

    Risolto limite di pagine/categorie/link (Fissato a 500). Correzioni minori su risultati incongrui da WP.

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,11 @@
+package test;
+
+import redis.clients.jedis.Jedis;
+
+public class Test {
+	public static void main (String [] args){
+		Jedis jedis = new Jedis ("127.0.0.1");
+		System.out.println(jedis.ping());
+		
+	}
+}
