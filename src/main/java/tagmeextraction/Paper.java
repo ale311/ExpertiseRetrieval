b7fe35046a9907ca9d4782f9c79384f28e8688ee
ale <ale@ale-archbook>
2016-03-04 16:44:50 +0100

    aggiunta classe Paper e relativa modifica dell'estrapolazione e memorizzazione dati TAGME

---
+++ src/main/java/tagmeextraction/Paper.java
@@ -0,0 +1,91 @@
+package tagmeextraction;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+class Paper {
+	HashSet<String> tag;
+	HashSet<String> categories;
+	HashSet<String> extract;
+	
+	public Paper(Set<String> t, Set<String> c, Set<String> e){
+		tag = (HashSet<String>) t;
+		categories = (HashSet<String>) c;
+		extract = (HashSet<String>) e;
+	}
+	public Paper(){
+		HashSet<String> tag = new HashSet<String>();
+		HashSet<String> categories = new HashSet<String>();
+		HashSet<String> extract = new HashSet<String>();
+		
+	}
+
+	public HashSet<String> getTag() {
+		return tag;
+	}
+
+	public void setTag(HashSet<String> tag) {
+		this.tag = tag;
+	}
+
+	public HashSet<String> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(HashSet<String> categories) {
+		this.categories = categories;
+	}
+
+	public HashSet<String> getExtract() {
+		return extract;
+	}
+
+	public void setExtract(HashSet<String> extract) {
+		this.extract = extract;
+	}
+
+	@Override
+	public String toString() {
+		return "Paper [tag=" + tag + ", categories=" + categories + ", extract=" + extract + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((categories == null) ? 0 : categories.hashCode());
+		result = prime * result + ((extract == null) ? 0 : extract.hashCode());
+		result = prime * result + ((tag == null) ? 0 : tag.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Paper other = (Paper) obj;
+		if (categories == null) {
+			if (other.categories != null)
+				return false;
+		} else if (!categories.equals(other.categories))
+			return false;
+		if (extract == null) {
+			if (other.extract != null)
+				return false;
+		} else if (!extract.equals(other.extract))
+			return false;
+		if (tag == null) {
+			if (other.tag != null)
+				return false;
+		} else if (!tag.equals(other.tag))
+			return false;
+		return true;
+	}
+	
+	
+}
--- src/main/java/tagmeextraction/TagMeExtraction.java
+++ src/main/java/tagmeextraction/TagMeExtraction.java
@@ -5,8 +5,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Locale.Category;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
@@ -15,18 +17,21 @@ import com.google.gson.JsonParser;
 
 public class TagMeExtraction {
 	public static void main (String[] args) throws IOException{
-		Set<String> titoli = getJsonDocument("Schumacher won the race in Indianapolis");
-		for(String s : titoli){
+		Paper cp = getPaperDescription("Schumacher won the race in Indianapolis");
+		for(String s : cp.extract){
 			System.out.println(s);
 		}
+		
 	}
 	
 	
 	public static Set<String> getJsonDocument(String text) throws IOException{
 		String text2 = text.replaceAll("\\W", "+");
+		String ab = "false";
 		text2.replace("\\n", "");
-		System.out.println(text2);
-		URL url = new URL("http://tagme.di.unipi.it/tag?text="+text2+"&key=8020b57e2d41b6041c4fd06937acbec7&long_text=true");
+//		System.out.println(text2);
+		
+		URL url = new URL("http://tagme.di.unipi.it/tag?text="+text2+"&key=8020b57e2d41b6041c4fd06937acbec7&long_text=true&include_categories=true&include_abstract=true");
 		HttpURLConnection request = (HttpURLConnection) url.openConnection();
 		request.connect();
 		HashSet<String> result = new HashSet<String>();
@@ -35,9 +40,65 @@ public class TagMeExtraction {
 		JsonArray array = pages.getAsJsonArray();
 		for(JsonElement ele : array){
 			String title = ele.getAsJsonObject().get("title").toString();
-			title = title.substring(1, title.length()-1);
-			result.add(title);
+			String extract = null;
+			JsonElement ar = ele.getAsJsonObject().get("dbpedia_categories");
+			for(JsonElement cat : ar.getAsJsonArray()){
+				System.out.println(cat.toString());
+			}
+			
+//			extract = ele.getAsJsonObject().get("abstract").toString();
+////			System.out.println(extract);
+//			title = title.substring(1, title.length()-1);
+//			result.add(title);
+			
 		}
 		return result;
 	}
+	
+	
+	
+	public static Paper getPaperDescription(String text) throws IOException{
+		Paper currentPaper = new Paper();
+		//preparo i 3 set che conterranno le stringhe che estraggo da tagme
+		HashSet<String> tag = new HashSet<String>();
+		HashSet<String> categories = new HashSet<String>();
+		HashSet<String> extract = new HashSet<String>();
+		//preparo il testo per fare l'estrazione del JSON da tagme
+		String text2 = text.replaceAll("\\W", "+");
+		text2.replace("\\n", "");
+		URL url = new URL("http://tagme.di.unipi.it/tag?text="+text2+"&key=8020b57e2d41b6041c4fd06937acbec7&long_text=true&include_categories=true&include_abstract=true");
+		HttpURLConnection request = (HttpURLConnection) url.openConnection();
+		request.connect();
+		HashSet<String> result = new HashSet<String>();
+		JsonElement jsonElement = new JsonParser().parse(new InputStreamReader((InputStream) request.getContent()));
+		//ho ottenuto il JSON, prendo solo l'elemento annotations che contiene tutti i dati di mio interesse
+		JsonElement pages = jsonElement.getAsJsonObject().get("annotations");
+		JsonArray array = pages.getAsJsonArray();
+		//estraggo le varie componenti e le inserisco nei giusti insiemi
+		for(JsonElement ele : array){
+			String title = ele.getAsJsonObject().get("title").toString();
+			tag.add(title.substring(1, title.length()-1));
+			String extr = ele.getAsJsonObject().get("abstract").toString();
+			extract.add(extr.substring(1, extr.length()-1));
+			JsonElement ar = ele.getAsJsonObject().get("dbpedia_categories");
+			for(JsonElement e : ar.getAsJsonArray()){
+//				System.out.println(cat.toString());
+				String cat = e.toString();
+				categories.add(cat.substring(1, cat.length()-1));
+			}
+			
+//			extract = ele.getAsJsonObject().get("abstract").toString();
+////			System.out.println(extract);
+//			title = title.substring(1, title.length()-1);
+//			result.add(title);
+			
+		}
+		currentPaper.setTag(tag);
+		currentPaper.setCategories(categories);
+		currentPaper.setExtract(extract);
+		
+		return currentPaper;
+	}
+	
+	
 }
